c7f2115521e10d12c5f1d2844c4da7183d598298
Miran <mso32@cam.ac.uk>
2023-03-13 16:30:26 +0000

    fedavgAngle maintains an adjacency matrix

--- fedavg_angle.py
+++ fedavg_angle.py
@@ -57,7 +57,20 @@ class FedAvgAngle(FedAvg):
             fit_metrics_aggregation_fn =fit_metrics_aggregation_fn,
             evaluate_metrics_aggregation_fn =evaluate_metrics_aggregation_fn,
         )
+        self.current_global_model = parameters_to_ndarrays(initial_parameters)
         self.angles = {}
+
+    def initialize_parameters(self, client_manager: ClientManager
+                                ) -> Optional[Parameters]:
+        """initialize the angles and adjacency matrix and then call the default FedAvg implementation of initialize_parameters"""
+        num_total_clients = client_manager.num_available()
+        self.adjacency_matrix = []
+        for i in range(num_total_clients):
+            connections = []
+            for j in range(num_total_clients):
+                connections.append(False)
+            self.adjacency_matrix.append(connections)
+        return super().initialize_parameters(client_manager = client_manager)
 
     def _get_angle(self, parameters1, parameters2):
         """
@@ -78,6 +91,44 @@ class FedAvgAngle(FedAvg):
         p2_normed = parameters2 / np.linalg.norm(parameters2)
         return np.arccos(np.clip(np.dot(p1_normed, p2_normed), -1.0, 1.0))
 
+    def configure_fit(self, server_round: int, parameters: Parameters, client_manager: ClientManager
+                      ) -> List[Tuple[ClientProxy, FitIns]]:
+        """stores the global model and then calls the original FedAvg configure_fit
+        """
+        self.current_global_model = parameters_to_ndarrays(parameters)
+        return super().configure_fit(server_round=server_round, parameters=parameters, client_manager=client_manager)
+
+    def _get_update(self, original_model : List[np.ndarray], updated_model : List[np.ndarray]) -> List[np.ndarray]:
+        """ extracts model update given the original and the new model
+        """
+        return [layer_new - layer_old for layer_old, layer_new in zip(original_model, updated_model)]
+
+    def _reset_adjacency_matrix(self):
+        """ 
+        Sets all elements of the adjacency matrix to False
+        """
+        for i in range(len(self.adjacency_matrix)):
+            for j in range(len(self.adjacency_matrix[i])):
+                self.adjacency_matrix[i][j] = False
+
+    def update_adjacency_matrix(self):
+        """
+        Updates the adjacency matrix based on the angles between the client updates
+        two clients are defined as connected if their average angle over different rounds is smaller than the median
+        """
+        self._reset_adjacency_matrix()
+        angle_list = list(self.angles.items())
+        angle_list.sort(key = lambda x : np.mean(x[1]))
+        for i in range(len(angle_list)//2):
+            c1_str, c2_str = angle_list[i][0]
+            try:
+                c1 = int(c1_str)
+                c2 = int(c2_str)
+            except:
+                raise ValueError("Client ids must be integers (with datatype str)")
+            self.adjacency_matrix[c1][c2] = True
+            self.adjacency_matrix[c2][c1] = True
+
     def aggregate_fit(
         self,
         server_round: int,
@@ -97,15 +148,17 @@ class FedAvgAngle(FedAvg):
             for _, fit_res in results
         ]
 
+        #store all the pairwise angles between the updates
         for i in range(len(results)):
             for j in range(i+1, len(results)):
-                angle = self._get_angle(parameters_to_ndarrays(results[i][1].parameters), parameters_to_ndarrays(results[j][1].parameters))
+                angle = self._get_angle(self._get_update(self.current_global_model, parameters_to_ndarrays(results[i][1].parameters)), self._get_update(self.current_global_model,parameters_to_ndarrays(results[j][1].parameters)))
                 cid_i = results[i][0].cid
                 cid_j = results[j][0].cid
                 key = (min(cid_i, cid_j), max(cid_i, cid_j))
                 existing_angles = self.angles.get(key, [])
                 existing_angles.append(angle)
                 self.angles[key] = existing_angles
+        self.update_adjacency_matrix()
 
         parameters_aggregated = ndarrays_to_parameters(aggregate(weights_results))
 
@@ -117,4 +170,5 @@ class FedAvgAngle(FedAvg):
         elif server_round == 1:  # Only log this warning once
             log(WARNING, "No fit_metrics_aggregation_fn provided")
 
+        self.current_global_model = parameters_aggregated
         return parameters_aggregated, metrics_aggregated
