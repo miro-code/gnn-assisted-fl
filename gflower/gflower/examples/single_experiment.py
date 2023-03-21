9d4ab54e3b111bfc3319ad9762ad78d34edbe3e2
Miran <mso32@cam.ac.uk>
2023-03-21 21:14:52 +0000

    parameter passing fix strategy

--- gflower/gflower/examples/single_experiment.py
+++ gflower/gflower/examples/single_experiment.py
@@ -176,14 +176,14 @@ def run_fixed_fl(
     parameters=default_parameters,
     **kwargs
 ):
+    parameters: Dict = {**parameters, **kwargs}
     strategy_dict = {
             "FedAvg" : FedAvg,
             "GCNAvg" : GCNAvg,
             "GCNAngleAvg" : GCNAngleAvg
         }
     strategy_class = strategy_dict[parameters["strategy"]]
-    parameters: Dict = {**parameters, **kwargs}
-
+    
     on_fit_config_fn: Callable[[int], Dict[str, Scalar]] = lambda cid: parameters[
         "train_config"
     ]
@@ -230,7 +230,7 @@ def run_fixed_fl(
         config=ServerConfig(num_rounds=parameters["num_rounds"]),
         strategy=strategy,
         seed=parameters["seed"],
-        name=f"lda_run_strategy_{parameters['strategy']}_clients_per_round_{parameters['num_clients_per_round']}_{parameters['seed']}"
+        name=f"single_experiment_run_strategy_{parameters['strategy']}_clients_per_round_{parameters['num_clients_per_round']}_{parameters['seed']}"
     )
 
-run_fixed_fl(num_clients_per_round = 10, seed = 42, num_total_clients = 3229, strategy = "GCNAngleAvg")
+run_fixed_fl(num_clients_per_round = 2, seed = 42, num_total_clients = 3229, strategy = "GCNAngleAvg")
